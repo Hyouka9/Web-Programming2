2f438cffdfa334cb151958053a2838a23971ea64
Hyouka9 <julianrifaldi63@gmail.com>
2021-09-18 08:00:44 +0700

    first commit

---
+++ contoh2.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+    //menghitung volume kubus
+    $sisi=15;
+    $v_kubus=$sisi*$sisi*$sisi;
+    $teks1="Belajar Menghitung ";
+    $teks2="Volume Kubus";
+    $hasil=$teks1 . $teks2;
+
+    echo "$hasil<br>";
+    echo "Panjang Sisi Kubus=$sisi<br>";
+    echo "Volume Kubus=$v_kubus cm3";
+?>
+</body>
+</html>
